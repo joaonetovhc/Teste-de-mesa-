23287194fecb0ecf8e9077f32e9dbf47467951dc
Joao Neto <joaodooleo11@gmail.com>
2023-08-22 13:47:35 -0300

    teste_mesa

---
+++ exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class TesteMesa{
+
+    static void Main(){
+
+        int a = 2;
+        int [] V = new int [6];
+
+        while(a < 6){
+            V[a] = 10 * a;
+            a++;
+
+          //  Console.WriteLine(V[a]);
+
+        }
+
+            for(int i = 0; i < V.Length; i++){
+                Console.Write($"{V[i]},");
+            }
+    }
+
+}
